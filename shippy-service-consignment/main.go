64fb422d00816f4da665fc45ad5530c12705e75c
Pedro Alves Batista <pedro.expb@gmail.com>
2020-09-25 23:09:17 -0300

    Start go mod

---
+++ shippy-service-consignment/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"sync"
+
+	pb "github.com/pedroalvesbatista/newshippy/shippy-service-consignment/proto/consignment"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+type repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+}
+
+type Repository struct {
+	mu           sync.RWMutex
+	consignments []*pb.Consignment
+}
+
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func main() {
+
+	repo := &Repository{}
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	pb.RegisterShippingServiceServer(s, &service{repo})
+
+	reflection.Register(s)
+
+	log.Println("Running on port: ", port)
+	if err != nil {
+		log.Fatal("Failed to serve: %v", err)
+	}
+}
