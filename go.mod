fd6824d97b6821b9c8abf78766e8e5a0f23cfa39
Pedro Alves Batista <pedro.expb@gmail.com>
2020-10-12 00:46:02 -0300

    Update files

--- go.mod
+++ go.mod
@@ -1,5 +1,14 @@
-module github.com/pedroalvesbatista/newshippy/shippy-service-consignment
+module github.com/pedroalvesbatista/shippy/shippy-service-consignment
 
-replace github.com/pedroalvesbatista/newshippy/shippy-service-consignment => ./shippy-service-consignment
+replace github.com/pedroalvesbatista/shippy/shippy-service-consignment => ../shippy-service-consignment
 
 go 1.14
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	golang.org/x/net v0.0.0-20201010224723-4f7140c49acb // indirect
+	golang.org/x/sys v0.0.0-20201009025420-dfb3f7c4e634 // indirect
+	google.golang.org/genproto v0.0.0-20201009135657-4d944d34d83c // indirect
+	google.golang.org/grpc v1.33.0
+	google.golang.org/protobuf v1.25.0 // indirect
+)
--- shippy-service-consignment/main.go
+++ shippy-service-consignment/main.go
@@ -1,3 +1,4 @@
+// shippy-service-consignment/main.go
 package main
 
 import (
@@ -6,6 +7,7 @@ import (
 	"net"
 	"sync"
 
+	// Import the generated protobuf code
 	pb "github.com/pedroalvesbatista/newshippy/shippy-service-consignment/proto/consignment"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -19,18 +21,43 @@ type repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
 }
 
+// Repository - Dummy repository, this simulates the use of a datastore
+// of some kind. We'll replace this with a real implementation later on.
 type Repository struct {
 	mu           sync.RWMutex
 	consignments []*pb.Consignment
 }
 
+// Create a new consignment
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.mu.Lock()
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	repo.mu.Unlock()
+	return consignment, nil
+}
+
+// Service should implement all of the methods to satisfy the service
+// we defined in our protobuf definition. You can check the interface
+// in the generated code itself for the exact method signatures etc
+// to give you a better idea.
+type service struct {
+	repo repository
+}
+
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an
+// argument, these are handled by the gRPC server.
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
 
+	// Save our consignment
 	consignment, err := s.repo.Create(req)
 	if err != nil {
 		return nil, err
 	}
 
+	// Return matching the `Response` message we created in our
+	// protobuf definition.
 	return &pb.Response{Created: true, Consignment: consignment}, nil
 }
 
@@ -38,19 +65,23 @@ func main() {
 
 	repo := &Repository{}
 
+	// Set-up our gRPC server.
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
+		log.Fatalf("failed to listen: %v", err)
 	}
-
 	s := grpc.NewServer()
 
+	// Register our service with the gRPC server, this will tie our
+	// implementation into the auto-generated interface code for our
+	// protobuf definition.
 	pb.RegisterShippingServiceServer(s, &service{repo})
 
+	// Register reflection service on gRPC server.
 	reflection.Register(s)
 
-	log.Println("Running on port: ", port)
-	if err != nil {
-		log.Fatal("Failed to serve: %v", err)
+	log.Println("Running on port:", port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
 }
